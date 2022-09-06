32e7d3c7f227b53f9755a031092def8bd55d156e
peekois <94543472+peekois@users.noreply.github.com>
2022-09-06 23:10:47 +0300

    Calculator and palindromes exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, item) => {
+    return total + item;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((total, item) => {
+    return total * item;
+  });
 };
 
-const power = function() {
-	
-};
+const power = (a, b) => a ** b;
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  const array = [];
+  for (i = 1; i <= num; i++) {
+    array.push(i);
+  }
+  return array.reduce(function (a, b) {
+    return a * b;
+  }, 1);
 };
 
 // Do not edit below this line
@@ -29,5 +37,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (input) {
+  let string = input.toLowerCase();
+  let array = Array.from(string);
+  let reverseArray = array.reverse();
 
+  if (reverseArray.toString() != array.toString()) {
+    return false;
+  }
+
+  if (reverseArray.toString() == array.toString()) {
+    return true;
+  }
 };
 
 // Do not edit below this line
