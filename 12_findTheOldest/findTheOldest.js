da17ac1b47eedb886ef2d8f57f2dba79866ca400
peekois <94543472+peekois@users.noreply.github.com>
2022-09-07 21:49:02 +0300

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (input) {
+  const sortedArray = input.sort(function (a, b) {
+    const currentYear = new Date().getFullYear();
+    const lastPerson = a.yearOfDeath !== undefined ? a.yearOfDeath - a.yearOfBirth : currentYear - a.yearOfBirth;
+    const nextPerson = b.yearOfDeath !== undefined ? b.yearOfDeath - b.yearOfBirth : currentYear - b.yearOfBirth;
+    return lastPerson > nextPerson ? -1 : 1;
+  });
+  return sortedArray[0];
 };
-
+// RAY, JANE, CARLY
 // Do not edit below this line
 module.exports = findTheOldest;
